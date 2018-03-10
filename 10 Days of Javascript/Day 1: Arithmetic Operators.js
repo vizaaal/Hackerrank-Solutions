274338a18afed1e186d5387035084a1c07271292
Vishal Shivapujimath <vishalshivapujimath@gmail.com>
2018-03-10 12:14:31 +0530

    Create Day 1: Arithmetic Operators.js

---
+++ 10 Days of Javascript/Day 1: Arithmetic Operators.js
@@ -0,0 +1,21 @@
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    
+    return perimeter;
+}
