349478bf350acb30c3e8e6689354863d6f229584
Vishal Shivapujimath <vishalshivapujimath@gmail.com>
2018-03-29 19:48:10 +0530

    Create Day 7: Pearson Correlation Coefficient I.py

---
+++ 10 Days of Statistics/Day 7: Pearson Correlation Coefficient I.py
@@ -0,0 +1,29 @@
+def _ss(data):
+    """Return sum of square deviations of sequence data."""
+    c = sum(data)/len(data)
+    ss = sum((x-c)**2 for x in data)
+    return ss
+
+def pstdev(data):
+    """Calculates the population standard deviation."""
+    n = len(data)
+    if n < 2:
+        raise ValueError('variance requires at least two data points')
+    ss = _ss(data)
+    pvar = ss/n # the population variance
+    return pvar**0.5
+
+elems = int(raw_input())
+xs = map(float, raw_input().split())
+ys = map(float, raw_input().split())
+
+mean_xs = sum(xs)/elems
+mean_ys = sum(ys)/elems
+
+sd_xs = pstdev(xs)
+sd_ys = pstdev(ys)
+
+p = sum([ (xs[i] - mean_xs) * (ys[i] - mean_ys) for i in xrange(elems) ])
+p = p / (elems * sd_xs * sd_ys)
+
+print round(p, 3)
