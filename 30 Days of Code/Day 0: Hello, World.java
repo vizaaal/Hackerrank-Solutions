b15ec1107979f73a245534a0803ed76a1e93d61d
Vishal Shivapujimath <vishalshivapujimath@gmail.com>
2018-04-07 12:22:26 +0530

    Create Day 0: Hello, World.java

---
+++ 30 Days of Code/Day 0: Hello, World.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class day0
+{
+	public static void main(String[] args) 
+	{
+	Scanner scan = new Scanner(System.in);
+	String inputString = scan.nextLine();
+	scan.close();
+	System.out.println("Hello, World.\n" + inputString);
+	}
+}
