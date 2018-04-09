1c3c59611fc58f1eba01321a0bd87e3635dc1865
Vishal Shivapujimath <vishalshivapujimath@gmail.com>
2018-04-09 17:34:05 +0530

    Create Day 2: Operators.java

---
+++ 30 Days of Code/Day 2: Operators.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+
+        double tip = mealCost * tipPercent / 100;
+        double tax = mealCost * taxPercent / 100;
+
+        int totalCost = (int) Math.round(mealCost + tip + tax);
+
+        System.out.println("The total meal cost is " + totalCost + " dollars.");
+    }
+}
